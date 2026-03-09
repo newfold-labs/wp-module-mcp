eedafac6ac74ec78826cac777c4bb8f3f20d53d3
zlateska <hzlateska@gmail.com>
2026-03-09 12:42:27 -0400

    Fix phpcs warning

--- tests/wpunit/McpValidationWPUnitTest.php
+++ tests/wpunit/McpValidationWPUnitTest.php
@@ -20,7 +20,7 @@ class McpValidationWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
 		$admin_id = $this->factory()->user->create( array( 'role' => 'administrator' ) );
 		wp_set_current_user( $admin_id );
 
-		$request = new \WP_REST_Request();
+		$request   = new \WP_REST_Request();
 		$validator = new McpValidation( $request );
 
 		$this->assertTrue( $validator->is_authenticated() );
@@ -34,7 +34,7 @@ class McpValidationWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
 	public function test_is_authenticated_returns_false_when_no_auth_header() {
 		wp_set_current_user( 0 );
 
-		$request = new \WP_REST_Request();
+		$request   = new \WP_REST_Request();
 		$validator = new McpValidation( $request );
 
 		$this->assertFalse( $validator->is_authenticated() );
